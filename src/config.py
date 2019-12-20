e716f4410f094ea4fd252253208271f79a9e5c34
SAMIA-CLOUD <sma225@njit.edu>
2019-12-20 04:24:51 -0500

    fix: config.py

--- src/config.py
+++ src/config.py
@@ -19,6 +19,7 @@ class Production(object):
     TESTING = False
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+
 class Testing(object):
     """
     Development environment configuration
@@ -27,6 +28,9 @@ class Testing(object):
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_TEST_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS=False
+
+
+
 app_config = {
     'development': Development,
     'production': Production,
